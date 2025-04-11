3cfddf5a0d0e3d8d12f6e18163d6fd5390a82ce5
youssefbouraoui1 <bouraouiyoussef12@gmail.com>
2025-04-12 00:25:35 +0100

    setting up the project

---
+++ Authentification/src/test/java/net/bouraouiechaib/authentification/AuthentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bouraouiechaib.authentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
